90c0592771b9cd31dd4623614daced03b2b00de1
Pawel Drozdz <paweldrozdz.91@interia.pl>
2019-03-24 21:29:02 +0100

    Added print template function for containers

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "print.hpp"
 #include "tuple.hpp"
+#include "print_template.hpp"
 // #include "template_parameter_list.hpp"
 
 #include <iostream>
+#include <vector>
+#include <map>
 
-int main()
+void test_print()
 {
     std::cout << "#### print1 ###\n";
     print1("Hello", "World", 1, 2.4);
@@ -21,14 +24,31 @@ int main()
     std::cout << "\n#### (f1(), f2()) ###\n";
     int ret = (f1(), f2());
     std::cout << "Ret: " << ret << "\n";
+}
 
-    // value_holder<int, char, int> holder;
-    // auto t = holder.apply;
+void test_tuple()
+{
     tuple<double, uint64_t, const char*> t1(12.2, 42, "big");
 
     std::cout << "0th elem is " << get<0>(t1) << "\n";
     std::cout << "1st elem is " << get<1>(t1) << "\n";
     std::cout << "2nd elem is " << get<2>(t1) << "\n";
+}
+
+void test_print_template()
+{
+    std::vector<int> vec {1, 2, 3, 4, 5};
+    std::map<int, std::string> m {{1, "one"}, {2, "two"}, {3, "three"}};
+
+    print_container(vec);
+    print_container(m);
+}
+
+int main()
+{
+    test_print();
+    test_tuple();
+    test_print_template();
 
     return 0;
 }
