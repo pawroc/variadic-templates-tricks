d7b2830b7e80c1b27122f043ab860cca48eae58a
Pawel Drozdz <pawel.drozdz@nokia.com>
2019-03-25 14:04:06 +0100

    Modified is_valid template in order to use parameter pack

--- sfinae14.cpp
+++ sfinae14.cpp
@@ -64,14 +64,14 @@ private:
     // We use std::declval to 'recreate' an object of 'UnnamedType'.
     // We use std::declval to also 'recreate' an object of type 'Param'.
     // We can use both of these recreated objects to test the validity!
-    template <typename Param> constexpr auto test_validity(int /* unused */)
-    -> decltype(std::declval<UnnamedType>()(std::declval<Param>()), std::true_type())
+    template <typename... Params> constexpr auto test_validity(int /* unused */)
+    -> decltype(std::declval<UnnamedType>()(std::declval<Params>()...), std::true_type())
     {
         // If substitution didn't fail, we can return a true_type.
         return std::true_type();
     }
 
-    template <typename Param> constexpr std::false_type test_validity(...)
+    template <typename... Params> constexpr std::false_type test_validity(...)
     {
         // Our sink-hole returns a false_type.
         return std::false_type();
@@ -80,15 +80,15 @@ private:
 public:
     // A public operator() that accept the argument we wish to test onto the UnnamedType.
     // Notice that the return type is automatic!
-    template <typename Param> constexpr auto operator()(const Param& p)
+    template <typename... Params> constexpr auto operator()(Params&& ...)
     {
         // The argument is forwarded to one of the two overloads.
         // The SFINAE on the 'true_type' will come into play to dispatch.
-        return test_validity<Param>(int());
+        return test_validity<Params...>(int());
     }
 };
 
-template <typename UnnamedType> constexpr auto is_valid(const UnnamedType& t)
+template <typename UnnamedType> constexpr auto is_valid(UnnamedType&& t)
 {
     return container<UnnamedType>();
 }
